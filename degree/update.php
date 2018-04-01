84f821398d60281bcf4d0a69361c06c363ead6c6
parthbabu <parth.babu05@gmail.com>
2018-04-01 20:33:08 +0530

    updated admin2

--- degree/update.php
+++ degree/update.php
@@ -36,12 +36,18 @@ $_dname=$row["deg_name"];
        <tr><td> Enter Id:<td>     <input type="text" value="<?php echo $_id; ?>" name="txtid" class="form-control" id="text" placeholder="Enter Id" required  >
         </tr><br></div>
     </div>
-    <div class="row">
+   <!-- <div class="row">
         <div class="form-group col-ld-10">
             <tr><td>Enter Degree:<td><input type="text" value="<?php echo $_dname ?>" name="txtdname" class="form-control" id="text" placeholder="Enter Degree " required >
         </tr></div>
+    </div>-->
+    <div class="row">
+    <tr><td>Degree:<td><select name="txtdname">
+    <option name="4"  value="<?php echo $_dname ?>"></option>
+<option name="1"  value="Mbbs">Mbbs</option>
+<option name="2" value="Md">Md</option>
+<option name="3" value="DMD">DMD</option>
     </div>
-    
     
     <div class="row">
         <tr><td><button type="submit" class="form-control btn btn-success" aria-label="Left Align"> Edit     
--- specialist/update.php
+++ specialist/update.php
@@ -36,13 +36,20 @@ $_sname=$row["spec_name"];
             <tr><td>Enter ID:<td><input type="text" value="<?php echo $_id; ?>" name="txtid" class="form-control" id="text" placeholder="Enter Id" required  >
         </tr><br></div>
     </div>
-    <div class="row">
+    <!--<div class="row">
         <div class="form-group col-ld-10">
             <tr><td>Enter Specialist:<td><input type="text" value="<?php echo $_sname ?>" name="txtsname" class="form-control" id="text" placeholder="Enter Specialist Name" required >
         </tr><br></div>
+    </div>-->
+    <div class="row">
+    <tr><td>Specialist:<td><select name="txtsname">
+    <option name="1"  value="<?php echo $_sname ?>"></option>
+<option name="1"  value="child specialist">chaild specialist</option>
+<option name="2" value="dentist">dentist</option>
+<option name="3" value="surgon">surgon</option>
+<option name="5" value="cardiologist">cardiologist</option>
+</select>
     </div>
-    
-    
     <div class="row">
        <tr><td> <button type="submit" class="form-control btn btn-success" aria-label="Left Align"> Edit     
             <span class="glyphicon glyphicon-check" ></span>
